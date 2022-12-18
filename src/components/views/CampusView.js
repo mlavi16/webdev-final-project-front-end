2a77fe8ea2a6756ac7b9293adf6dd4bf97903d21
mlavi16 <maya.lavi11@gmail.com>
2022-12-18 16:26:47 +0200

    fixing no students enrolled message

--- src/components/views/CampusView.js
+++ src/components/views/CampusView.js
@@ -9,26 +9,32 @@ import { Link } from "react-router-dom";
 
 // Take in props data to construct the component
 const CampusView = (props) => {
-  const {campus} = props;
+  const { campus } = props;
 
   // Render a single Campus view with list of its students
-  if (!campus.students.length) {
-    return (
+    // <p>There are no students.</p>
+  return (
     <div>
-      <p>There are no students.</p>
+      <h1>{campus.name}</h1>
+      <img src={campus.imageUrl} alt={campus.name} width="200px" />
+      <p>{campus.address}</p>
+      <p>{campus.description}</p>
+      <StudentsList students={campus.students}/>
       <Link to={`/newstudent`}>
         <button>Add New Student</button>
       </Link>
     </div>
-    );
+  );
+};
+
+const StudentsList = ({students}) => {
+  // const { campus } = props;
+  if (!students.length) {
+    return (<h3>There are no students enrolled here.</h3>)
   } else {
     return (
       <div>
-        <h1>{campus.name}</h1>
-        <img src={campus.imageUrl} alt={campus.name} width="200px"/>
-        <p>{campus.address}</p>
-        <p>{campus.description}</p>
-        {campus.students.map( student => {
+        {students.map(student => {
           let name = student.firstname + " " + student.lastname;
           return (
             <div key={student.id}>
@@ -38,12 +44,9 @@ const CampusView = (props) => {
             </div>
           );
         })}
-        <Link to={`/newstudent`}>
-          <button>Add New Student</button>
-        </Link>
       </div>
-    );
+    )
   }
-};
+}
 
 export default CampusView;
